eae02578d5a948df62827ebbf82adaadf0ef6abd
koeun23 <goeun1235@gmail.com>
2022-09-05 00:02:46 +0900

    0904commit
    글 조회 가능

---
+++ final_project/src/main/java/com/kosta/finalproject/repository/FindCarBoardRepository.java
@@ -0,0 +1,9 @@
+package com.kosta.finalproject.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.kosta.finalproject.model.FindCarBoard;
+
+public interface FindCarBoardRepository  extends JpaRepository<FindCarBoard, Integer> {
+
+}
